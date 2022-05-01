7f3e9fcbb5ba9ce09831fb95e16b963b8c736780
smtsuchi <shoha1999@gmail.com>
2022-05-01 19:36:51 -0400

    ready to host on heroku

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,6 +12,10 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 
 from pathlib import Path
 
+import os
+from dotenv import load_dotenv
+load_dotenv()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,12 +24,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-vb!huysj!i8p^!0^-u#(zeiv#@sph-*q%cg6uy4tcxr_1!pz#+'
+SECRET_KEY = os.getenv('SECRET_KEY') or 'django-insecure-vb!huysj!i8p^!0^-u#(zeiv#@sph-*q%cg6uy4tcxr_1!pz#+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'immortalsliondance.com', 'quiet-coast-24655.herokuapp.com', '.immortalslionsdance.com']
 
 
 # Application definition
@@ -41,6 +45,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
